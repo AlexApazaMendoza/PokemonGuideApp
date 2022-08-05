8838027bde51ed99c946b1d8a9d5932a634d3393
Alex Apaza <alex.apaza@tismart.com>
2022-08-05 13:08:04 -0500

    add okhhtp interceptor

--- app/src/main/java/com/example/pokemonguideapp/retrofit/RetrofitConfig.kt
+++ app/src/main/java/com/example/pokemonguideapp/retrofit/RetrofitConfig.kt
@@ -1,13 +1,21 @@
 package com.example.pokemonguideapp.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitConfig {
+
+    private val logging: HttpLoggingInterceptor by lazy {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
     private val retrofit : Retrofit by lazy {
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("https://pokeapi.co/api/v2/")
+            .client(OkHttpClient.Builder().addInterceptor(logging).build())
             .build()
     }
 
