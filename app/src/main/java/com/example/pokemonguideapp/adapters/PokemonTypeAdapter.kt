28b18715a5aa33f03a13dcb658dfddf3e78ab84e
Alex Apaza <alex.apaza@tismart.com>
2022-09-30 15:53:19 -0500

    add detail dialog pokemon

--- app/src/main/java/com/example/pokemonguideapp/adapters/PokemonTypeAdapter.kt
+++ app/src/main/java/com/example/pokemonguideapp/adapters/PokemonTypeAdapter.kt
@@ -33,6 +33,29 @@ class PokemonTypeAdapter(private val dataSet:MutableList<Type>, )
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(holder){
             binding.tvTypePokemonTitle.text = dataSet[position].type.name
+            binding.root.setBackgroundResource(
+                when(dataSet[position].type.name){
+                    "steel" -> R.color.color_type_pokemon_steel
+                    "water" -> R.color.color_type_pokemon_water
+                    "bug" -> R.color.color_type_pokemon_bug
+                    "dragon" -> R.color.color_type_pokemon_dragon
+                    "electric" -> R.color.color_type_pokemon_electric
+                    "ghost" -> R.color.color_type_pokemon_ghost
+                    "fire" -> R.color.color_type_pokemon_fire
+                    "fairy" -> R.color.color_type_pokemon_fairy
+                    "ice" -> R.color.color_type_pokemon_ice
+                    "fighting" -> R.color.color_type_pokemon_fighting
+                    "normal" -> R.color.color_type_pokemon_normal
+                    "grass" -> R.color.color_type_pokemon_grass
+                    "psychic" -> R.color.color_type_pokemon_psychic
+                    "dark" -> R.color.color_type_pokemon_dark
+                    "ground" -> R.color.color_type_pokemon_ground
+                    "poison" -> R.color.color_type_pokemon_poison
+                    "flying" -> R.color.color_type_pokemon_flying
+                    "rock" -> R.color.color_type_pokemon_rock
+                    else -> R.color.white
+                }
+            )
         }
     }
 
--- app/src/main/java/com/example/pokemonguideapp/dialogs/PokemonDetailDialog.kt
+++ app/src/main/java/com/example/pokemonguideapp/dialogs/PokemonDetailDialog.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.pokemonguideapp.R
 import com.example.pokemonguideapp.adapters.PokemonAdapter
 import com.example.pokemonguideapp.adapters.PokemonTypeAdapter
 import com.example.pokemonguideapp.databinding.PokemonDetailDialogBinding
@@ -28,6 +29,7 @@ class PokemonDetailDialog(var pokemon: PokemonResponse): DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        dialog!!.window?.setBackgroundDrawableResource(R.drawable.round_corner);
         mBinding = PokemonDetailDialogBinding.inflate(inflater,container, false)
         return mBinding.root
     }
@@ -45,6 +47,30 @@ class PokemonDetailDialog(var pokemon: PokemonResponse): DialogFragment() {
             orderPokemon.text = "#${pokemon.order.toString().padStart(3,'0')}"
             tvHeightPokemon.text = pokemon.height.toString().toHeightPokemonDisplay()
             tvWeightPokemon.text = pokemon.weight.toString().toWeightPokemonDisplay()
+
+            root.setBackgroundResource(
+                when(pokemon.types.first().type.name){
+                    "steel" -> R.color.color_type_pokemon_steel_light
+                    "water" -> R.color.color_type_pokemon_water_light
+                    "bug" -> R.color.color_type_pokemon_bug_light
+                    "dragon" -> R.color.color_type_pokemon_dragon_light
+                    "electric" -> R.color.color_type_pokemon_electric_light
+                    "ghost" -> R.color.color_type_pokemon_ghost_light
+                    "fire" -> R.color.color_type_pokemon_fire_light
+                    "fairy" -> R.color.color_type_pokemon_fairy_light
+                    "ice" -> R.color.color_type_pokemon_ice_light
+                    "fighting" -> R.color.color_type_pokemon_fighting_light
+                    "normal" -> R.color.color_type_pokemon_normal_light
+                    "grass" -> R.color.color_type_pokemon_grass_light
+                    "psychic" -> R.color.color_type_pokemon_psychic_light
+                    "dark" -> R.color.color_type_pokemon_dark_light
+                    "ground" -> R.color.color_type_pokemon_ground_light
+                    "poison" -> R.color.color_type_pokemon_poison_light
+                    "flying" -> R.color.color_type_pokemon_flying_light
+                    "rock" -> R.color.color_type_pokemon_rock_light
+                    else -> R.color.white
+                }
+            )
         }
 
         context?.let {
@@ -56,7 +82,7 @@ class PokemonDetailDialog(var pokemon: PokemonResponse): DialogFragment() {
         }
 
         mPokemonTypeAdapter = PokemonTypeAdapter(pokemon.types.toMutableList())
-        mGridLayout = GridLayoutManager(requireContext(),2)
+        mGridLayout = GridLayoutManager(requireContext(), if(pokemon.types.size > 1) 2 else 1)
 
         mBinding.rvTypePokemon.apply {
             setHasFixedSize(true)
