3bdc9a06e874bf9fd7176d2240d773db75663869
Alex <alexapazamendoza@gmail.com>
2022-10-24 15:52:06 -0500

    integration firebase

--- app/src/main/java/com/example/pokemonguideapp/MainActivity.kt
+++ app/src/main/java/com/example/pokemonguideapp/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.pokemonguideapp.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 
     //
     private lateinit var navController: NavController
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +33,36 @@ class MainActivity : AppCompatActivity() {
         //
         navController = Navigation.findNavController(this,R.id.navGraphMainHostFragment)
         setupWithNavController(mBinding.bottomNav,navController)
+
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            finish()
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 }
